6b7a01b64e7d79521f35579c91e2bec50466907d
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-25 20:59:26 +0100

    chore: update commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,22 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['fix', 'test', 'tooling', 'refactor', 'revert', 'example', 'docs', 'format', 'feat', 'chore', 'ci']
-    ]
-  }
-}
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"fix",
+				"test",
+				"tooling",
+				"refactor",
+				"revert",
+				"example",
+				"docs",
+				"format",
+				"feat",
+				"chore",
+				"ci",
+			],
+		],
+	},
+};

eb17716ba5da0daa7814e5c1c428029be1f86a2e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-08-25 22:54:43 +0100

    chore: update metadata & build entrypoints

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: {
     index: "src/index.ts",
+    filelogger: "src/filelogger.ts",
   },
   format: ["esm"],
   target: "node20",
